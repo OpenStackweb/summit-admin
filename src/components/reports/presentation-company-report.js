5b72de453869dd7949b4747101037a570cc35e6f
Santi <santipalenque@gmail.com>
2019-04-24 15:18:30 -0300

    WIP

--- src/components/reports/presentation-company-report.js
+++ src/components/reports/presentation-company-report.js
@@ -15,7 +15,7 @@ import React from 'react'
 import T from 'i18n-react/dist/i18n-react'
 import { Pagination } from 'react-bootstrap';
 import { Breadcrumb } from 'react-breadcrumbs';
-import { Table } from 'openstack-uicore-foundation/lib/components'
+import { Table, FreeTextSearch } from 'openstack-uicore-foundation/lib/components'
 import {getReport} from "../../actions/report-actions";
 import {connect} from "react-redux";
 const Query = require('graphql-query-builder');
@@ -26,21 +26,40 @@ class PresentationCompanyReport extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            searchTerm: null,
+            sortKey: null,
+            sortDir: null
+        };
+
         this.buildQuery = this.buildQuery.bind(this);
         this.handleSort = this.handleSort.bind(this);
         this.handlePageChange = this.handlePageChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleExportReport = this.handleExportReport.bind(this);
     }
 
-    componentWillMount () {
-        let query = this.buildQuery(1);
+    componentDidMount () {
+        if (!this.props.currentSummit) return;
 
+        let query = this.buildQuery(1);
         this.props.getReport(query, reportName, 1);
     }
 
+    componentWillReceiveProps (newProps) {
+        if (!newProps.currentSummit) return;
+        if (this.props.currentSummit.id != newProps.currentSummit.id) {
+            this.setState({searchTerm: null});
+            let query = this.buildQuery(1);
+            this.props.getReport(query, reportName, 1);
+        }
+    }
 
-    buildQuery(page, key=null, dir=null) {
-        let {pageInfo, currentPage, perPage} = this.props;
-        let filters = {first: 25, summit_Id: 25};
+
+    buildQuery(page) {
+        let {pageInfo, currentPage, perPage, currentSummit} = this.props;
+        let {searchTerm, sortKey, sortDir} = this.state;
+        let filters = {first: 25, summit_Id: currentSummit.id};
 
         if (page != 1) {
             if(page > currentPage) {
@@ -52,9 +71,13 @@ class PresentationCompanyReport extends React.Component {
             }
         }
 
-        if (key) {
-            let order = (dir == 1) ? '' : '-';
-            filters.sortBy = order + '' + key;
+        if (sortKey) {
+            let order = (sortDir == 1) ? '' : '-';
+            filters.sortBy = order + '' + sortKey;
+        }
+
+        if (searchTerm) {
+            filters.search = searchTerm;
         }
 
 
@@ -69,7 +92,7 @@ class PresentationCompanyReport extends React.Component {
         member.find(["id", "firstName", "lastName","email", {"affiliations": affiliations}]);
         let category = new Query("category");
         category.find(["id", "title"]);
-        let presentations = new Query("presentations",{summit_Id: 25});
+        let presentations = new Query("presentations",{summit_Id: currentSummit.id});
         presentations.find([{"edges":[{"node":["id", "title", "abstract", {"category": category}]}]}]);
 
         query.find([
@@ -103,8 +126,11 @@ class PresentationCompanyReport extends React.Component {
                 break;
         }
 
-        let query = this.buildQuery(1, key, dir);
-        this.props.getReport(query, reportName, 1);
+        this.setState({sortKey: sortKey, sortDir: dir}, () => {
+            let query = this.buildQuery(1);
+            this.props.getReport(query, reportName, 1);
+        });
+
     }
 
     handlePageChange(page) {
@@ -113,8 +139,20 @@ class PresentationCompanyReport extends React.Component {
 
     }
 
+    handleSearch(term) {
+        this.setState({searchTerm: term}, () => {
+            let query = this.buildQuery(1);
+            this.props.getReport(query, reportName, 1);
+        });
+    }
+
+    handleExportReport() {
+        console.log("export");
+    }
+
     render() {
         let {data, pageInfo, match, currentPage, totalCount, perPage} = this.props;
+        let {searchTerm} = this.state;
 
         let report_columns = [
             { columnKey: 'link', value: 'Link' },
@@ -155,6 +193,21 @@ class PresentationCompanyReport extends React.Component {
                         <h3>{T.translate(`reports.${reportName}`)}</h3>
                     </div>
 
+                </div>
+                <hr/>
+                <div className={'row'}>
+                    <div className={'col-md-6'}>
+                        <FreeTextSearch
+                            value={searchTerm}
+                            placeholder={T.translate("reports.placeholders.search")}
+                            onSearch={this.handleSearch}
+                        />
+                    </div>
+                    <div className="col-md-6 text-right">
+                        <button className="btn btn-primary right-space" onClick={this.handleExportReport}>
+                            {T.translate("reports.export")}
+                        </button>
+                    </div>
                 </div>
                 <hr/>
 
