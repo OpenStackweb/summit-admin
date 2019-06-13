2764803790960bde648c1bd4eaa819d49cf94121
Sebastian Marcet <smarcet@gmail.com>
2019-06-13 19:49:20 -0300

    [smarcet] * fixed loop on access token expiration

--- src/routes/authorization-callback-route.js
+++ src/routes/authorization-callback-route.js
@@ -10,125 +10,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-
-import URI from "urijs"
-import React from 'react'
-import { Route, Redirect } from 'react-router-dom'
 import { withRouter } from 'react-router-dom'
-import IdTokenVerifier from 'idtoken-verifier'
 import history from '../history'
+import React from 'react'
+import { connect } from 'react-redux';
+import { AbstractAuthorizationCallbackRoute } from "openstack-uicore-foundation/lib/components";
+import { getUserInfo } from "openstack-uicore-foundation/lib/methods";
 
-class AuthorizationCallbackRoute extends React.Component {
+class AuthorizationCallbackRoute extends AbstractAuthorizationCallbackRoute {
 
     constructor(props){
-        console.log("AuthorizationCallbackRoute::AuthorizationCallbackRoute");
-        super(props);
-        this.state = {
-            access_token: null ,
-            id_token: null,
-            session_state: null,
-            error: null,
-            error_description: null,
-        };
+        super(process.env['IDP_BASE_URL'], process.env['OAUTH2_CLIENT_ID'], props);
     }
 
-    extractHashParams() {
-        return URI.parseQuery(this.props.location.hash.substr(1));
-    }
-
-    validateIdToken(idToken){
-        let verifier = new IdTokenVerifier({
-            issuer:   process.env['IDP_BASE_URL'],
-            audience: process.env['OAUTH2_CLIENT_ID']
-        });
-        let storedNonce = window.localStorage.getItem('nonce');
-        window.localStorage.removeItem('nonce');
-        console.log(`retrieved nonce ${storedNonce}`);
-        let jwt    = verifier.decode(idToken);
-        let alg    = jwt.header.alg;
-        let kid    = jwt.header.kid;
-        let aud    = jwt.payload.aud;
-        let iss    = jwt.payload.iss;
-        let exp    = jwt.payload.exp;
-        let nbf    = jwt.payload.nbf;
-        let tnonce = jwt.payload.nonce || null;
-        return tnonce == storedNonce && aud == process.env['OAUTH2_CLIENT_ID'] && iss == process.env['IDP_BASE_URL'];
-    }
-
-    componentWillMount() {
-        console.log("AuthorizationCallbackRoute::componentWillMount");
-        let { access_token , id_token, session_state, error, error_description } = this.extractHashParams();
-        this.setState({...this.state, access_token, id_token, session_state, error ,error_description});
-    }
-
-    componentDidMount() {
-        console.log("AuthorizationCallbackRoute::componentDidMount");
-        let {access_token, id_token, session_state, error, error_description } = this.state;
-        if(error){
-            return;
-        }
-        let { getUserInfo } = this.props;
-
-
-        this.props.onUserAuth(access_token, id_token, session_state);
-        let url              = URI( window.location.href);
-        let query            = url.search(true);
-        let fragment         = URI.parseQuery(url.fragment());
-
-        // purge fragment
-        delete fragment['access_token'];
-        delete fragment['expires_in'];
-        delete fragment['token_type'];
-        delete fragment['scope'];
-        delete fragment['id_token'];
-        delete fragment['session_state'];
-
-        let backUrl = query.hasOwnProperty('BackUrl') ? query['BackUrl'] : '/app';
-
-        if (fragment.length > 0) {
-            backUrl     += `#${URI.buildQuery(fragment)}`;
-        }
-
-        console.log("backUrl is "+backUrl);
-
-        getUserInfo(backUrl, history);
-
-    }
-
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return false;
-    }
-
-    render() {
-        console.log("AuthorizationCallbackRoute::render");
-        let { getUserInfo } = this.props;
-        let {access_token, id_token, error, error_description } = this.state;
-
-        if(error){
-            console.log("AuthorizationCallbackRoute::render - access_token is null");
-
-            return (
-                <Route render={ props => {
-                    return <Redirect to={`/error?error=${error}&error_description=${error_description}`} />
-                }} />
-            )
-        }
-
-        if(!this.validateIdToken(id_token))
-        {
-            console.log("AuthorizationCallbackRoute::render - not valid id_token");
-            let tmp_error = "validation error";
-            let tmp_error_description = "invalid id token";
-            return (
-                <Route render={ props => {
-                    return <Redirect to={`/error?error=${tmp_error}&error_description=${tmp_error_description}`} />
-                }} />
-            )
-        }
-
-        return null;
+    _callback(backUrl) {
+        this.props.getUserInfo(backUrl, history);
     }
 }
 
-export default withRouter(AuthorizationCallbackRoute);
+const mapStateToProps = ({ loggedUserState }) => ({
+    accessToken: loggedUserState.accessToken,
+    idToken:  loggedUserState.idToken,
+    sessionState: loggedUserState.sessionState,
+})
+
+export default connect(mapStateToProps,{
+    getUserInfo
+})(AuthorizationCallbackRoute)
 
