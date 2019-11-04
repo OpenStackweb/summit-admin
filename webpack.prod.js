0b380f65dd13b08951362e25a8e7be31ebe7f678
Santi <santipalenque@gmail.com>
2019-11-04 13:54:32 -0300

    update uicore version to 2.0 and improve webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,7 +8,6 @@ const path                      = require('path');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map',
     output: {
         filename: '[name]_[hash].js',
         path: path.resolve(__dirname, 'dist'),
@@ -20,6 +19,9 @@ module.exports = merge(common, {
             new TerserJSPlugin({terserOptions: {compress: {inline: false}}}),
             new OptimizeCSSAssetsPlugin({})
         ],
+        splitChunks: {
+            chunks: 'all'
+        }
     },
     plugins: [
         new MiniCssExtractPlugin({
