e574f11b6350455dc81fa798288efb0f90fd6ed8
Santi <santipalenque@gmail.com>
2020-10-09 14:03:01 -0300

    fix room report date sorting

--- src/components/reports/room-report.js
+++ src/components/reports/room-report.js
@@ -110,8 +110,11 @@ class RoomReport extends React.Component {
 
         let processedData = flatData.map(it => {
 
-            let momentStartDate = moment(it.startDate).tz(currentSummit.time_zone_id);
-            let momentEndDate = moment(it.endDate).tz(currentSummit.time_zone_id);
+            // 2020-10-19T12:30:00+00:00
+
+            const format = 'YYYY-MM-DDTHH:mm:ss+00:00';
+            let momentStartDate = moment(it.startDate, format).tz(currentSummit.time_zone_id);
+            let momentEndDate = moment(it.endDate, format).tz(currentSummit.time_zone_id);
 
             let date = momentStartDate.format('dddd Do');
             let date_simple = momentStartDate.format('YYYY-M-D');
