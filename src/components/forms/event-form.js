b90ba392156c4929d4068fb1e2cd67e4c6fd1f30
smarcet <smarcet@gmail.com>
2021-05-10 12:03:46 -0300

    Fixed materials typo
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/components/forms/event-form.js
+++ src/components/forms/event-form.js
@@ -712,7 +712,7 @@ class EventForm extends React.Component {
                         </div>
                     </div>
                 </Panel>
-                {entity.id !== 0 && this.isEventType('Presentation') &&
+                {entity.id != 0 && this.isEventType('PresentationType') &&
                 <Panel show={showSection === 'materials'} title={T.translate("edit_event.materials")}
                        handleClick={this.toggleSection.bind(this, 'materials')}>
                     <button className="btn btn-primary pull-right left-space" onClick={this.handleNewMaterial}>
