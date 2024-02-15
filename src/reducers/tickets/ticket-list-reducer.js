b4f68c90d62c70aad5ee1374a478ca2f1b42d25e
Tomás Castillo <tomrndom@users.noreply.github.com>
2024-02-15 12:18:38 -0300

    Check missing names on ticket list for assignee (#365)
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/reducers/tickets/ticket-list-reducer.js
+++ src/reducers/tickets/ticket-list-reducer.js
@@ -98,7 +98,7 @@ const ticketListReducer = (state = DEFAULT_STATE, action) => {
                     order_id: t.order.id,
                     ticket_type: t.ticket_type ? t.ticket_type.name : 'N/A',
                     bought_date: bought_date,
-                    owner_name: t.owner ? t.owner.first_name + ' ' + t.owner.last_name : 'N/A',
+                    owner_name: t.owner && (t.owner.first_name && t.owner.last_name) ? t.owner.first_name + ' ' + t.owner.last_name : 'N/A',
                     owner_email: t.owner ? t.owner.email : 'N/A',
                     owner_company: t.owner &&  t.owner.company ? t.owner.company : '',
                     promo_code: t.promo_code ? t.promo_code.code : 'N/A',
